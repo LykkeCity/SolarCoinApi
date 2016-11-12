b5f33b89ea8a71ac390ea8890d1d0f7438055a24
nikkkaforum <nikagamkrelidze233@gmail.com>
2016-11-12 14:31:40 +0400

    versioning patch added, /home/version added

---
+++ src/SolarCoinApi.Facade/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolarCoinApi.Facade.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Version()
+        {
+            return typeof(HomeController).GetTypeInfo().Assembly.GetName().Version.ToString();
+        }
+    }
+}
